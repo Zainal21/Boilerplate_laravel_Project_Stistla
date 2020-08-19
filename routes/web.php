067ec71655df48bc800ed4e4ccc30d4402e5007e
Muhamad Zainal Arifin <zainalarifin080718@gmail.com>
2020-08-20 00:14:52 +0700

    update route web

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,7 @@ Route::group([
     Route::get('admin/setting', 'SettingController@index');
     // user
     Route::get('admin/users', 'UserController@index');
-
+        
     Route::get('admin/users/create', 'UserController@create');
     
     Route::get('admin/users/edit', 'UserController@edit');
