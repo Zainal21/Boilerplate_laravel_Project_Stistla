77e9474bc3697443c98247a310b575af9411cda0
Muhamad Zainal Arifin <zainalarifin080718@gmail.com>
2020-09-18 15:02:46 +0700

    update laravel version

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Cp\DashboardController;
+use App\Http\Controllers\Cp\AuthController;
+use App\Http\Controllers\Cp\SocialMediaController;
+use App\Http\Controllers\Cp\PostController;
+use App\Http\Controllers\Cp\UserController;
+use App\Http\Controllers\Cp\SettingController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,28 +20,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function(){
+  return view('welcome');
 });
 
+Route::group(
+  ['prefix' => 'site'],
+   function(){
 
-Route::group([
-    'prefix' => 'site/',
-     'namespace' => 'Cp'
-    ], function(){
-    Route::get('auth', 'AuthController@login');
-    
-    Route::get('admin', 'DashboardController@index');
-    Route::resource('admin/posts', 'PostController');
-    Route::resource('admin/social_media', 'SocialMediaController');
-
-    Route::get('admin/message', 'SocialMediaController@message');
-    // setting
-    Route::get('admin/setting', 'SettingController@index');
-    // user
-    Route::get('admin/users', 'UserController@index');
-        
-    Route::get('admin/users/create', 'UserController@create');
-    
-    Route::get('admin/users/edit', 'UserController@edit');
+  Route::get('login', [AuthController::class, 'login']);
+  Route::get('admin', [DashboardController::class, 'index']);
+  Route::resource('admin/posts', PostController::class);
+  Route::resource('admin/social_media', SocialMediaController::class);
+  Route::get('admin/message',[SocialMediaController::class,'message']);
+  // setting
+  Route::get('admin/setting', [SettingController::class,'index']);
+  // user
+  Route::get('admin/users', [UserController::class,'index']);
+  Route::get('admin/users/create', [UserController::class,'create']);
+  Route::get('admin/users/edit', [UserController::class, 'edit']);
 });
